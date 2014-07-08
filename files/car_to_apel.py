0be21f9d00c8873e9217142a149e60faf9e867f8
fschaer <frederic.schaer@cea.fr>
2014-07-08 11:04:08 +0200

    work around python bug

--- files/car_to_apel.py
+++ files/car_to_apel.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 #
-# This sends the ARC CAR accounting reports into a python dirqueue (this needs python-dirq), 
-# which then allows the apel dbloader to process this dirqueue directly and send the records 
+# This sends the ARC CAR accounting reports into a python dirqueue (this needs python-dirq),
+# which then allows the apel dbloader to process this dirqueue directly and send the records
 # into an apel accounting database, into the JobRecords table
 #
-# CAR accounting records are then TAR-archived into a subfolder, and failed records moved 
+# CAR accounting records are then TAR-archived into a subfolder, and failed records moved
 # as is into another "failed" subfolder, so that nothing's lost in the process.
 #
 # Copyright : F.SCHAER
@@ -17,6 +17,8 @@ import datetime
 import tarfile
 import shutil
 import ConfigParser
+import logging
+logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s',level=logging.INFO)
 
 #def fixNS(x):
 #  for i in x.childNodes:
@@ -44,10 +46,10 @@ if config.has_section(ARC_archives_section) and config.has_option(ARC_archives_s
             arcdir=val
 
 if not arcdir:
-    print "ARC " + ARC_archives_option + " option in " + ARC_archives_section + " section of "+ARC_config+" does not seem to contain an archiving dir suboption : cannot parse that dir for apel. exiting."
+    logging.error("ARC " + ARC_archives_option + " option in " + ARC_archives_section + " section of "+ARC_config+" does not seem to contain an archiving dir suboption : cannot parse that dir for apel. exiting.")
     exit(2)
 
-print "Processing ARC archives in " + arcdir + "..."
+logging.info("Processing ARC archives in " + arcdir + "...")
 
 # init directories and filenames
 archives=os.path.join(arcdir,"archives")
@@ -60,19 +62,20 @@ for root, dirs, files in os.walk(arcdir,topdown=False):
      for name in dirs:
          fname = os.path.join(root,name)
          if re.compile('[0-9]{8,8}').match(name) and not os.listdir(fname): #to check wither the dir is empty and of the form 01234567 (the dirq tmp dirs)
-             print "removing emtpty dir " + fname
+             logging.info("removing emtpty dir " + fname)
              os.removedirs(fname)
 
 #then, make sure we don't remove CAR records, just archive them
 if not os.path.isdir(archives):
-  os.mkdir(archives)
+    os.mkdir(archives)
 if not os.path.isdir(failures):
-  os.mkdir(failures)
+    os.mkdir(failures)
 
 #init structures and archive file, chdir
 dirq = Queue(arcdir, schema=QSCHEMA)
-tarball = tarfile.open(tarfileN,mode='a')
+tarball = tarfile.open(tarfileN,mode='a:')
 tarmembers = tarball.getnames()
+processed=0
 
 try:
   os.chdir(arcdir)
@@ -101,14 +104,21 @@ try:
                  "empaid": "unknown"
                  })
           if filename not in tarmembers:
-            #print 'adding ' + filename + ' to ' + tarfileN
+            logging.debug('adding ' + filename + ' to ' + tarfileN)
             tarball.add(filename) #add file to archive
             os.remove(filename)   #remove CAR file that was just archived, so that it's not added again in the dirq next time we run
           else:
-            print "WARNING : file " + filename + " already in archive " + tarfileN + " : something might be going wrong. Moving it in archives/failed"
+            logging.warnging("file " + filename + " already in archive " + tarfileN + " : something might be going wrong. Moving it in archives/failed")
             shutil.move(filename,failures)
+          #mark record as processed
+          processed+=1
 finally:
-  tarball.close()
-  os.chdir(previousDir)
+    numrecords=len(tarball.getnames());
+    tarball.close()
+    os.chdir(previousDir)
+    logging.debug("number of tarball members : " + str(numrecords))
+    if numrecords==0 and processed==0:
+        logging.info('did not process any record, and tarball is empty : removing it to avoir a python bug.')
+        os.remove(tarfileN)
 
 exit(0)
